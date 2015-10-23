81a4cb3ed52e5c116eb247cdb372f2b124a584f8
Adrian Arnason <struckthenerve@gmail.com>
2015-10-24 00:51:36 +0200

    Basic car and controls (keyboard + joystick)

---
+++ AbandonGame/Assets/Scripts/CarController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarController : MonoBehaviour {
+    public int motorMultiplier;
+    public int steerAngle;
+    public int breakForce;
+
+    private Rigidbody rigidB;
+    [SerializeField] private WheelCollider wheelColFL;
+    [SerializeField] private WheelCollider wheelColFR;
+    [SerializeField] private WheelCollider wheelColRL;
+    [SerializeField] private WheelCollider wheelColRR;
+    private float currentMotorTorque;
+    private float currentSteerAngle;
+
+    // Use this for initialization
+    void Start () {
+        rigidB = gameObject.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        currentMotorTorque = Input.GetAxis("Forward") * motorMultiplier;
+        currentMotorTorque -= Input.GetAxis("Reverse") * motorMultiplier;
+
+        currentSteerAngle = Input.GetAxis("Steer");
+
+        wheelColRL.motorTorque = currentMotorTorque;
+        wheelColRR.motorTorque = currentMotorTorque;
+
+        if(Input.GetButtonDown("Break"))
+        {
+            wheelColRL.brakeTorque = breakForce;
+            wheelColRR.brakeTorque = breakForce;
+        }
+        else if(Input.GetButtonUp("Break"))
+        {
+            wheelColRL.brakeTorque = 0;
+            wheelColRR.brakeTorque = 0;
+        }
+
+        wheelColFL.steerAngle = currentSteerAngle * steerAngle;
+        wheelColFR.steerAngle = currentSteerAngle * steerAngle;
+    }
+}
