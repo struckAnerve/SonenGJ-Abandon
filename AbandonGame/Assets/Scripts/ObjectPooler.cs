a26a5444ea8d591b8fe7ed3dbc94f662bca5246a
morbo1993 <geokar12@nith.no>
2015-10-23 22:31:12 +0200

    Added a scene for making environment. Added an object pooler to scripts.

---
+++ AbandonGame/Assets/Scripts/ObjectPooler.cs
@@ -0,0 +1,111 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ObjectPooler.cs" company="Trollpants Game Studios AS">
+//  Copyright (c) 2014 All Rights Reserved
+// </copyright>
+// <summary>
+//  This script will spawn and recycle game objects so the game can reuse objects,
+//  instead of always creating new ones and destroying them. This helps maintain good performance.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Trollpants.UtilityBelt
+{
+    using UnityEngine;
+
+    public class ObjectPooler : UnitySingleton<ObjectPooler>
+    {
+        #region Fields & properties
+
+        public GameObject[] objectPool;
+
+        private Transform _objectPoolTransform;
+
+
+        #endregion /Fields & properties
+
+        #region Public methods
+
+        /// <summary>
+        /// Spawn an object from a prefab, the ObjectPooler will either instantiate a new object or reuse an old one
+        /// </summary>
+        /// <param name="prefabPath">The path where the prefab is stored, the prefab MUST be stored in the Resources folder
+        ///  and the string must conform to this example: "Prefabs/Enemies/"</param>
+        /// <param name="prefabName">The name of the prefab (filename without extension)</param>
+        /// <param name="parent">The parent of the new GameObject</param>
+        /// <param name="worldPosition">Where in world space the object will be spawned</param>
+        /// <returns>The spawned <see cref="GameObject"/></returns>
+        public GameObject Spawn(string prefabName, Transform parent, Vector3 worldPosition, Quaternion rotation)
+        {
+            var poolGameObject = _objectPoolTransform.FindChild(prefabName);
+            GameObject g = null;
+
+            if (poolGameObject != null)
+            {
+                g = poolGameObject.gameObject;
+                g.transform.position = worldPosition;
+                g.transform.rotation = rotation;
+            }
+            else
+            {
+                foreach (var prefab in objectPool)
+                {
+                    if (prefab.name == prefabName)
+                    {
+                        g = Instantiate(prefab, worldPosition, rotation) as GameObject;
+                        break;
+                    }
+                }
+            }
+
+            if (g != null)
+            {
+                g.name = prefabName;
+                var rigidBody = g.GetComponent<Rigidbody>();
+                rigidBody.velocity = Vector3.zero;
+                rigidBody.angularVelocity = Vector3.zero;
+
+                g.transform.SetParent(parent);
+                g.SetActive(true);
+                return g;
+            }
+
+#if DEBUG
+            Debug.LogError("Spawn failed!");
+#endif
+            return null;
+        }
+
+        #endregion /Public methods
+
+        #region Unity methods
+
+        private void OnSpeedChanged(SpeedChanged speedChangedEvent)
+        {
+            foreach (Transform movingObject in transform)
+            {
+                MovingObject m = movingObject.GetComponent<MovingObject>();
+                if (m != null)
+                {
+                    m.Speed = -speedChangedEvent.newPlayerSpeed;
+                }
+            }            
+        }
+
+        private void Awake()
+        {
+            _objectPoolTransform = gameObject.transform;
+            _objectPoolTransform.name = "ObjectPooler";
+        }
+
+        private void OnEnable()
+        {
+            Events.instance.AddListener<SpeedChanged>(OnSpeedChanged);
+        }
+
+        private void OnDisable()
+        {
+            Events.instance.RemoveListener<SpeedChanged>(OnSpeedChanged);
+        }
+        #endregion /Unity methods
+    }
+}
