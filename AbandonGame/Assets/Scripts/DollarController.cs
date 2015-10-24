9aa831bac21adebfa8eb1cfe4c31bb3030671fd2
Adrian Arnason <struckthenerve@gmail.com>
2015-10-25 01:07:32 +0200

    Fixed dollar

---
+++ AbandonGame/Assets/Scripts/DollarController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DollarController : MonoBehaviour {
+    public float rotationSpeed;
+    public Vector3 offset;
+    private GameObject abandoningPlayer;
+
+	
+    void OnEnable()
+    {
+        Events.instance.AddListener<AbandonerChanged>(OnAbandonerChanged);
+    }
+
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(0, Time.deltaTime * rotationSpeed, 0);
+        transform.position = abandoningPlayer.transform.position + offset;
+	}
+
+    private void OnAbandonerChanged(AbandonerChanged e)
+    {
+        abandoningPlayer = e.newAbandoner;
+    }
+
+    void OnDisable()
+    {
+        Events.instance.RemoveListener<AbandonerChanged>(OnAbandonerChanged);
+    }
+}
