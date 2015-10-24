c28c2404a9e1772b8f641bdf051883c4a22440be
Adrian Arnason <struckthenerve@gmail.com>
2015-10-24 14:55:55 +0200

    Tried to better handling

--- AbandonGame/Assets/Scripts/CarController.cs
+++ AbandonGame/Assets/Scripts/CarController.cs
@@ -13,35 +13,58 @@ public class CarController : MonoBehaviour {
     [SerializeField] private WheelCollider wheelColRR;
     private float currentMotorTorque;
     private float currentSteerAngle;
+    private float initialRearSidewaysFrictionStiffness;
+    private int currentBreakForce;
 
     // Use this for initialization
     void Start () {
         rigidB = gameObject.GetComponent<Rigidbody>();
         rigidB.centerOfMass = new Vector3(0, -0.5f, 0);
+
+        initialRearSidewaysFrictionStiffness = wheelColRL.sidewaysFriction.stiffness;
 	}
 	
-	// Update is called once per frame
-	void Update () {
+    void Update() {
         currentMotorTorque = Input.GetAxis("Forward") * motorMultiplier;
         currentMotorTorque -= Input.GetAxis("Reverse") * motorMultiplier;
 
         currentSteerAngle = Input.GetAxis("Steer");
 
+        if (Input.GetButtonDown("Break"))
+        {
+            currentBreakForce = breakForce;
+
+            WheelFrictionCurve frictionCurve = wheelColRL.sidewaysFriction;
+            frictionCurve.stiffness = 3.5f;
+            wheelColRL.sidewaysFriction = frictionCurve;
+            frictionCurve = wheelColRR.sidewaysFriction;
+            frictionCurve.stiffness = 3.5f;
+            wheelColRR.sidewaysFriction = frictionCurve;
+        }
+        else if (Input.GetButtonUp("Break"))
+        {
+            currentBreakForce = 0;
+
+            WheelFrictionCurve frictionCurve = wheelColRL.sidewaysFriction;
+            frictionCurve.stiffness = initialRearSidewaysFrictionStiffness;
+            wheelColRL.sidewaysFriction = frictionCurve;
+            frictionCurve = wheelColRR.sidewaysFriction;
+            frictionCurve.stiffness = initialRearSidewaysFrictionStiffness;
+            wheelColRR.sidewaysFriction = frictionCurve;
+        }
+    }
+
+    void FixedUpdate () {
         wheelColRL.motorTorque = currentMotorTorque;
         wheelColRR.motorTorque = currentMotorTorque;
 
-        if(Input.GetButtonDown("Break"))
-        {
-            wheelColRL.brakeTorque = breakForce;
-            wheelColRR.brakeTorque = breakForce;
-        }
-        else if(Input.GetButtonUp("Break"))
-        {
-            wheelColRL.brakeTorque = 0;
-            wheelColRR.brakeTorque = 0;
-        }
+        wheelColRL.brakeTorque = currentBreakForce;
+        wheelColRR.brakeTorque = currentBreakForce;
 
         wheelColFL.steerAngle = currentSteerAngle * steerAngle;
         wheelColFR.steerAngle = currentSteerAngle * steerAngle;
+
+        Debug.Log(rigidB.velocity.magnitude * 60 * 60 / 1000);
+        //rigidB.AddForce(Vector3.down * 100 * Vector3.Magnitude(rigidB.velocity));
     }
 }
