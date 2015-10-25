79d3f2886c893be18ea8381fadbba51d05e9d21c
MarieRoald <roald.marie@gmail.com>
2015-10-25 15:48:23 +0100

    Added tumbleweed spawning

---
+++ AbandonGame/Assets/Tumbleweed.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tumbleweed : SpawnableSuperclass {
+
+    // Use this for initialization
+
+    Rigidbody rb;
+
+    float MaxWindForce = 7f;
+    new void Start()
+    {
+        base.Start();
+        rb = thisSpawnableObj.GetComponentInChildren<Rigidbody>();
+    }
+    new void Update()
+    {
+        base.Update();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.AddTorque(Vector3.back * Random.Range(5, MaxWindForce));
+    }
+}
